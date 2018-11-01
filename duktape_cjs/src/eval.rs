ede8d9e718362ebbfa6b3cc3b6a054cf8fe6f1b2
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-11-01 23:39:40 +0100

    do not use mut

--- duktape_cjs/src/eval.rs
+++ duktape_cjs/src/eval.rs
@@ -5,10 +5,7 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-pub fn eval_main<'a, T: AsRef<Path>>(
-    ctx: &'a mut duktape::Context,
-    path: T,
-) -> error::Result<Object> {
+pub fn eval_main<'a, T: AsRef<Path>>(ctx: &'a duktape::Context, path: T) -> error::Result<Object> {
     let mut real_p = path.as_ref().to_path_buf();
     if !real_p.is_absolute() {
         real_p = env::current_dir()?.join(path);
@@ -18,7 +15,7 @@ pub fn eval_main<'a, T: AsRef<Path>>(
 }
 
 pub fn eval_main_script<'a, T: AsRef<Path>, S: AsRef<[u8]>>(
-    ctx: &'a mut duktape::Context,
+    ctx: &'a duktape::Context,
     path: T,
     script: S,
 ) -> error::Result<Object> {
