43b4d270bb2be2925883079da235281628aa74ef
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-11-28 17:39:54 +0100

    persist bindings

--- duktape_sys/build.rs
+++ duktape_sys/build.rs
@@ -1,24 +1,36 @@
-extern crate bindgen;
 extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h")
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
+    // let bindings = bindgen::Builder::default()
+    //     // The input header we would like to generate
+    //     // bindings for.
+    //     .header("wrapper.h")
+    //     // Finish the builder and generate the bindings.
+    //     .generate()
+    //     // Unwrap the Result and panic on failure.
+    //     .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    // // Write the bindings to the $OUT_DIR/bindings.rs file.
+    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    // bindings
+    //     .write_to_file(out_path.join("bindings.rs"))
+    //     .expect("Couldn't write bindings!");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+
+    #[cfg(feature = "buildtime_bindgen")]
+    {
+        bindings::write_to_out_dir("wrapper.h", &out_path);
+    }
+    #[cfg(not(feature = "buildtime_bindgen"))]
+    {
+        use std::fs;
+        fs::copy("bindings/duktape_binding.rs", out_path)
+                .expect("Could not copy bindings to output directory");
+    }
 
     let mut builder = cc::Build::new();
 
@@ -33,4 +45,63 @@ fn main() {
     }
 
     builder.compile("libduktape.a");
+}
+
+
+
+// #[cfg(not(feature = "buildtime_bindgen"))]
+// mod bindings {
+
+//     use std::path::{Path};
+//     use std::fs;
+
+//     pub fn write_to_out_dir(_header: &str, out_path: &Path) {
+//         let in_path = "binding/duktape_binding.rs";
+//         fs::copy(in_path, out_path).expect("Could not copy bindings to output directory");
+//     }
+// }
+
+
+#[cfg(feature = "buildtime_bindgen")]
+mod bindings {
+
+    extern crate bindgen;
+
+    use std::path::{Path};
+    use std::fs;
+
+
+    pub fn write_to_out_dir(header: &str, out_path: &Path) {
+        let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(header)
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
+
+    fs::create_dir("bindings").unwrap_or(());
+    fs::copy(out_path, "bindings/duktape_binding.rs").expect("could not copy bindings");
+
+    // let mut builder = cc::Build::new();
+
+    // builder
+    //     .file("duktape-2.3.0/src/duktape.c")
+    //     .flag_if_supported("-fomit-frame-pointer")
+    //     .flag_if_supported("-fstrict-aliasing");
+    // // .flag_if_supported("-fprofile-generate")
+    // let profile = env::var("PROFILE").unwrap();
+    // if profile == "release" {
+    //     builder.opt_level(2);
+    // }
+
+    // builder.compile("libduktape.a");
+    }
 }
