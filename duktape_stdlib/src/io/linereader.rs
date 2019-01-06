62d614f3437427210a8765ac1675616607f6d89c
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2019-01-06 20:52:38 +0100

    line reader

---
+++ duktape_stdlib/src/io/linereader.rs
@@ -0,0 +1,55 @@
+use super::readers::{ReadAllFn, ReadFn};
+use super::writers::{FlushFn, WriteFn};
+use duktape::prelude::*;
+use super::types::get_line_reader;
+
+pub(crate) struct ReadLineFn;
+
+impl class::Method for ReadLineFn {
+    fn call(&self, ctx: &Context, this: &mut class::Instance) -> DukResult<i32> {
+        let reader = get_line_reader(ctx, this)?;
+
+        let mut buffer = String::new();
+        match reader.read_line(&mut buffer) {
+            Err(e) => duk_error!(format!("error while reading: {}", e)),
+            Ok(_) => {}
+        };
+
+        ctx.push(buffer)?;
+
+        Ok(1)
+    }
+}
+
+pub(crate) fn build_linereader<'a>(ctx: &'a Context, parent: Function<'a>) -> DukResult<Function<'a>> {
+    let mut readwriter = class::build();
+
+    readwriter
+        .name("LineReader")
+        .inherit(parent)
+        .method(
+            "readLine",
+            (1, |_ctx: &Context, _this: &mut class::Instance| {
+                duk_error!("Don't use the Reader class directly")
+            }),
+        );
+
+    let readwriter = ctx.push(readwriter)?.getp()?;
+
+    Ok(readwriter)
+}
+
+pub(crate) fn build_linereader_class<'a>(
+    ctx: &'a Context,
+    linereader: Function<'a>,
+) -> DukResult<Function<'a>> {
+    let mut builder = class::build();
+
+    builder
+        .inherit(linereader)
+        .method("read", ReadFn {})
+        .method("readLine", ReadLineFn {})
+        .method("readAll", ReadAllFn {});
+
+    Ok(ctx.push(builder)?.getp()?)
+}
